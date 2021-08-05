82a003640afb84ce6d1033386c9cfde65dd15d66
BuithanhtungHUB <buithanhtung100496@gmail.com>
2021-08-05 22:49:20 +0700

    fix route, loginController, Register

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,12 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::prefix('admin')->group(function () {
+
+
+
+
+});
+Route::prefix('admin')->group(function (){
     Route::get('login', [LoginController::class, 'showFormLogin'])->name('admin.showFromlogin');
     Route::post('login', [LoginController::class, 'login'])->name('admin.login');
     Route::get('create', [UserController::class, 'create'])->name('users.create');
@@ -37,32 +43,31 @@ Route::prefix('admin')->group(function () {
         });
         Route::get('logout',[LoginController::class, 'logout'])->name('admin.logout');
 
+        Route::prefix('product')->group(function (){
+            Route::get('/',[ProductController::class,'index'])->name('product.list');
+            Route::get('/create',[ProductController::class,'create'])->name('product.create');
+            Route::post('/create',[ProductController::class,'store'])->name('product.store');
+            Route::get('/update/{id}',[ProductController::class,'edit'])->name('product.edit');
+            Route::post('/update/{id}',[ProductController::class,'update'])->name('product.update');
+            Route::get('/delete/{id}',[ProductController::class,'destroy'])->name('product.delete');
+        });
+        Route::prefix('brand')->group(function (){
+            Route::get('/',[BrandController::class,'index'])->name('brand.list');
+            Route::get('/create',[BrandController::class,'create'])->name('brand.create');
+            Route::post('/create',[BrandController::class,'store'])->name('brand.store');
+            Route::get('/update/{id}',[BrandController::class,'edit'])->name('brand.edit');
+            Route::post('/update/{id}',[BrandController::class,'update'])->name('brand.update');
+            Route::get('/delete/{id}',[BrandController::class,'destroy'])->name('brand.delete');
+        });
+        Route::prefix('category')->group(function (){
+            Route::get('/',[CategoryController::class,'index'])->name('category.list');
+            Route::get('/create',[CategoryController::class,'create'])->name('category.create');
+            Route::post('/create',[CategoryController::class,'store'])->name('category.store');
+            Route::get('/update/{id}',[CategoryController::class,'edit'])->name('category.edit');
+            Route::post('/update/{id}',[CategoryController::class,'update'])->name('category.update');
+            Route::get('/delete/{id}',[CategoryController::class,'destroy'])->name('category.delete');
+        });
     });
 
-});
-Route::prefix('admin')->group(function (){
-    Route::prefix('product')->group(function (){
-        Route::get('/',[ProductController::class,'index'])->name('product.list');
-        Route::get('/create',[ProductController::class,'create'])->name('product.create');
-        Route::post('/create',[ProductController::class,'store'])->name('product.store');
-        Route::get('/update/{id}',[ProductController::class,'edit'])->name('product.edit');
-        Route::post('/update/{id}',[ProductController::class,'update'])->name('product.update');
-        Route::get('/delete/{id}',[ProductController::class,'destroy'])->name('product.delete');
-    });
-    Route::prefix('brand')->group(function (){
-        Route::get('/',[BrandController::class,'index'])->name('brand.list');
-        Route::get('/create',[BrandController::class,'create'])->name('brand.create');
-        Route::post('/create',[BrandController::class,'store'])->name('brand.store');
-        Route::get('/update/{id}',[BrandController::class,'edit'])->name('brand.edit');
-        Route::post('/update/{id}',[BrandController::class,'update'])->name('brand.update');
-        Route::get('/delete/{id}',[BrandController::class,'destroy'])->name('brand.delete');
-    });
-    Route::prefix('category')->group(function (){
-        Route::get('/',[CategoryController::class,'index'])->name('category.list');
-        Route::get('/create',[CategoryController::class,'create'])->name('category.create');
-        Route::post('/create',[CategoryController::class,'store'])->name('category.store');
-        Route::get('/update/{id}',[CategoryController::class,'edit'])->name('category.edit');
-        Route::post('/update/{id}',[CategoryController::class,'update'])->name('category.update');
-        Route::get('/delete/{id}',[CategoryController::class,'destroy'])->name('category.delete');
-    });
+
 });
